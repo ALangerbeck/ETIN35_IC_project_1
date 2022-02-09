36698d740d55f5e5c15fd181a0724d99a8d87fb6
Alfred Langerbeck <32991448+ALangerbeck@users.noreply.github.com>
2022-02-09 16:47:07 +0100

    Deleted vivado project

--- VivadoProject/IC_project_1/IC_project_1.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2016.1\\bin
-call %xv_path%/xelab  -wto 6104a0bc73364625952fe940f7187c0d -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot shift_reg_tb_behav xil_defaultlib.shift_reg_tb -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
