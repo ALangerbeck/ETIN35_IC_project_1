8a7f008ee8bd678f77344a46d6abd588b0ea6cc3
Tinnis97 <lina.tinnerberg@gmail.com>
2022-02-24 08:53:07 +0100

    Create TOP_TOP.vhd

---
+++ VHD_files/TOP_TOP.vhd
@@ -0,0 +1,53 @@
+
+
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+--Should the top top wrapper have any input or outputs? Or should they just map our signals to propper pads?
+
+entity TOP_TOP is 
+
+--how to write a port empty entity?
+
+end TOP_TOP;
+
+
+architecture TOP_TOP_arch of TOP_TOP is
+
+-- SIGNAL DEFINITIONS
+    signal clk_module : std_logic;
+    signal rst_module : std_logic;
+    signal input_module : std_logic_vector(7 downto 0);
+    signal valid_input_module : std_logic;
+    signal ready_module : std_logic;
+
+
+-- COMPONENT DEFINITION
+
+component TOP_matmult is 
+
+    port (  clk     : in std_logic;
+            rst     : in std_logic;
+            input   : in std_logic_vector(7 downto 0);
+            valid_input : in std_logic;
+            ready  : out std_logic
+         );
+
+end component;
+
+begin 
+
+TOP : TOP_matmult 
+
+    port map(  clk     => clk_module,
+            rst     => rst,
+            input   => input_module,
+            valid_input => valid_input_module,
+            ready  => ready_module
+         );
+
+
+
+end TOP_TOP_arch;
